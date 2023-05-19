ace262d6818dd8905e8f4566c61fb9cec31c52af
Fidan Haliti <fidan91haliti@gmail.com>
2023-05-20 00:31:22 +0200

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import CartIcon from "./CartIcon"
 
 const Header = ({cart}) => {
   return (
-    <div className="flex justify-between items-center mb-4 sticky top-0 bg-white ">
+    <div className="flex justify-between items-center mb-4 max-[600px]:sticky top-0 bg-white ">
       <h1 className="text-3xl font-semibold ">React and Tailwind CSS Shopping Cart</h1>
         <CartIcon cart={cart}/>
     
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,7 +3,7 @@ import productData from ".//productData";
 
 const Products = (props) => {
   return (
-    <div className="grid grid-cols-3 gap-3 mb-8 sm:grid grid-cols-1 ">
+    <div className="grid grid-cols-3 gap-3 mb-8 max-[600px]:grid-cols-1 ">
       {productData.map((product)=> (
         <ProductItem product={product} key={product.id} cart={props.cart}
          setCart={props.setCart} />
